699e19f95bbc80aa5ea6f174244f80c305f9980a
Brendon Guedes <53941150+brendonguedes@users.noreply.github.com>
2020-01-22 10:55:01 -0300

    upload files important!

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyled from './styles/global';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <>
+      <Routes />
+      <GlobalStyled />
+    </>
+  );
+}
+
+export default App;
